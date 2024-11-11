7b6fea8718260e0c7f8ed0412bd4f82c165c9891
“Abdul <kaazees31@gmail.com>
2024-11-11 21:57:34 +0530

    new

--- src/Context/auth.js
+++ src/Context/auth.js
@@ -6,27 +6,29 @@ const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({ user: null, token: "" });
 
-
-  axios.defaults.headers.common["Authorization"] = auth?.token;
-
+  // Set the Authorization header only after the auth state has been updated
+  useEffect(() => {
+    axios.defaults.headers.common["Authorization"] = auth?.token;
+  }, [auth.token]); // This will run whenever the auth.token changes
 
   useEffect(() => {
     const data = localStorage.getItem("auth");
     if (data) {
       const parse = JSON.parse(data);
       setAuth({
-        ...auth,
         user: parse.user,
         token: parse.token,
       });
     }
-   
-  }, []);
+  }, []); // This runs only once, on component mount
+
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
       {children}
     </AuthContext.Provider>
   );
 };
+
 const useAuth = () => useContext(AuthContext);
+
 export { useAuth, AuthProvider };
